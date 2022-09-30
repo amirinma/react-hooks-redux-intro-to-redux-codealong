5f3d7d699c5c14b691827196ccfcdc5e5cf56f2a
Mohammad <zia.ro@live.com>
2022-09-30 12:01:54 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import counterReducer from "./features/counter/counterSlice.js";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
+import {createStore} from "redux"
+const store = createStore(counterReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById("root"));
 
-ReactDOM.render(<App />, document.getElementById("root"));
